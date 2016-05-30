adb63c8184683ec927148979699c4b80bfd4b144
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-05-30 17:20:08 +0200

    generating regex protection

---
+++ regex_rules_test.js
@@ -0,0 +1,37 @@
+var elements = require('./regex_rules.json');
+
+var print = function (m1, m2) {
+  console.log(m1, m2);
+};
+var context = {
+  setVariable: function (s, v) {
+    console.log(s, v);
+  },
+  proxyRequest: {
+    url: 'http://localhost/bla/create function foobar returns'
+  }
+}
+
+var block = function (haystack, filters) {
+  filters.some(function (jsonRegex) {
+    // Create a regex from the json string.
+    print('regex',jsonRegex.rule);
+    var f = new RegExp(jsonRegex.rule, jsonRegex.flags);
+    // print('regex',f);
+    var hit = f.exec(haystack);
+    if (hit) {
+      print('found', hit[0]);
+      context.setVariable('FILTER.block', true);
+      return true;
+    }
+  });
+};
+
+elements.forEach(function (element) {
+  var filters = element.filters;
+  if (element.element === 'QueryParam') {
+    if (block(context.proxyRequest.url, filters)) {
+      return;
+    }
+  }
+});
