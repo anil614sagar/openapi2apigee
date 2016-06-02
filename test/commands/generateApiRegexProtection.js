0925e89f95b93c4637dcba81abc2972994ee858b
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-06-02 13:46:28 +0200

    tests...

--- test/commands/generateApiRegexProtection.js
+++ test/commands/generateApiRegexProtection.js
@@ -24,43 +24,53 @@ describe('generateApi with regex-protection', function() {
   });
 
   describe('Add regex-protection policy', function() {
-    // it('Headers token policy should be generated', function(done) {
-    //   var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/policies/add-headers-token.xml");
-    //   var file = fs.lstatSync(filePath);
-    //   should.equal(file.isFile(), true);
-    //
-    //   var fileData = fs.readFileSync(filePath);
-    //   var parser = new xml2js.Parser();
-    //   parser.parseString(fileData, function (err, result) {
-    //     result.should.have.property('AssignMessage');
-    //     result.should.have.property('AssignMessage').property('Set');
-    //     var headers = result.AssignMessage.Set[0].Headers[0];
-    //     // Check Header name and value
-    //     should.equal(headers.Header[0].$.name, 'x-token', 'x-token not found: ');
-    //     should.equal(headers.Header[0]._, 'random_token', 'x-token value not correct');
-    //     done();
-    //   });
-    // });
 
-    // it('Headers x-api-key policy should be generated', function(done) {
-    //   var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/policies/add-headers-x-api-key.xml");
-    //   var file = fs.lstatSync(filePath);
-    //   should.equal(file.isFile(), true);
-    //
-    //   var fileData = fs.readFileSync(filePath);
-    //   var parser = new xml2js.Parser();
-    //   parser.parseString(fileData, function (err, result) {
-    //     result.should.have.property('AssignMessage');
-    //     result.should.have.property('AssignMessage').property('Set');
-    //     var headers = result.AssignMessage.Set[0].Headers[0];
-    //     // Check Header name and value
-    //     should.equal(headers.Header[0].$.name, 'x-api-key', 'x-api-key not found: ');
-    //     should.equal(headers.Header[0]._, 'random_api_key', 'x-api-key value not correct');
-    //     done();
-    //   });
-    // });
+    it('Regexp protection policy should be generated', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/policies/regex-protection.xml");
+      var file = fs.lstatSync(filePath);
+      should.equal(file.isFile(), true);
 
-    it('Proxy should contain header step in PreFlow', function(done) {
+      var fileData = fs.readFileSync(filePath);
+      var parser = new xml2js.Parser();
+      parser.parseString(fileData, function (err, result) {
+        result.should.have.property('Javascript');
+        result.should.have.property('Javascript').property('ResourceURL');
+        // Check Header name and value
+        should.equal(result.Javascript.ResourceURL[0], 'jsc://regex-protection.js', 'regex protection script not found');
+        should.equal(result.Javascript.IncludeURL[0], 'jsc://regex.js', 'regex js script not found');
+        done();
+      });
+    });
+
+    it('Js files should be generated', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/resources/jsc/regex-protection.js");
+      var file = fs.lstatSync(filePath);
+      should.equal(file.isFile(), true);
+      var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/resources/jsc/regex.js");
+      var file = fs.lstatSync(filePath);
+      should.equal(file.isFile(), true);
+      done();
+    });
+
+    it('Raise fault policy should be generated', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/policies/raiseFault.xml");
+      var file = fs.lstatSync(filePath);
+      should.equal(file.isFile(), true);
+
+      var fileData = fs.readFileSync(filePath);
+      var parser = new xml2js.Parser();
+      parser.parseString(fileData, function (err, result) {
+        result.should.have.property('RaiseFault');
+        result.should.have.property('RaiseFault').property('FaultResponse');
+        // Check Header name and value
+        console.log('RaiseFault.FaultResponse[0]', result.RaiseFault.FaultResponse[0].Set[0].ReasonPhrase[0]);
+        should.equal(result.RaiseFault.FaultResponse[0].Set[0].ReasonPhrase[0], 'Bad Request', 'ReasonPhrase not found');
+        should.equal(result.RaiseFault.FaultResponse[0].Set[0].StatusCode[0], '501', '501 not found');
+        done();
+      });
+    });
+
+    it('Proxy should contain Add Regex Protection step in PreFlow', function(done) {
       var filePath = path.join(options.destination, options.apiProxy, '/apiproxy/proxies/default.xml');
       var fileData = fs.readFileSync(filePath);
       var parser = new xml2js.Parser();
@@ -68,6 +78,18 @@ describe('generateApi with regex-protection', function() {
         result.should.have.property('ProxyEndpoint');
         result.should.have.property('ProxyEndpoint').property('PreFlow');
         should.equal(result.ProxyEndpoint.PreFlow[0].Request[0].Step[0].Name[0], 'Add Regex Protection', 'Add Reqex Protection step in found in PreFlow');
+        done();
+      });
+    });
+
+    it('Proxy should contain Raise Regex Error step in PreFlow', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy, '/apiproxy/proxies/default.xml');
+      var fileData = fs.readFileSync(filePath);
+      var parser = new xml2js.Parser();
+      parser.parseString(fileData, function (err, result) {
+        result.should.have.property('ProxyEndpoint');
+        result.should.have.property('ProxyEndpoint').property('PreFlow');
+        should.equal(result.ProxyEndpoint.PreFlow[0].Request[0].Step[1].Name[0], 'Raise Regex Error', 'Raise Regex Error step in found in PreFlow');
         done();
       });
     });
