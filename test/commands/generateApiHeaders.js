3b9c3a337386a302e735d6dfbd246d00f1d66ca0
ronald (MacBookie Pro) <ronald@orangewise.com>
2016-07-27 12:21:11 +0200

    added tests for headers

--- test/commands/generateApiHeaders.js
+++ test/commands/generateApiHeaders.js
@@ -60,6 +60,30 @@ describe('generateApi with headers', function() {
       });
     });
 
+    it('Headers securit headers policy should be generated', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy + "/apiproxy/policies/add-headers-security.xml");
+      var file = fs.lstatSync(filePath);
+      should.equal(file.isFile(), true);
+
+      var fileData = fs.readFileSync(filePath);
+      var parser = new xml2js.Parser();
+      parser.parseString(fileData, function (err, result) {
+        result.should.have.property('AssignMessage');
+        result.should.have.property('AssignMessage').property('Set');
+        var headers = result.AssignMessage.Set[0].Headers[0];
+        // Check Header name and value
+        should.equal(headers.Header[0].$.name, 'Strict-Transport-Security', 'Strict-Transport-Security not found: ');
+        should.equal(headers.Header[0]._, 'max-age=31536000; includeSubDomains; preload', 'value not correct');
+        should.equal(headers.Header[1].$.name, 'X-XSS-Protection', 'X-XSS-Protection header not found: ');
+        should.equal(headers.Header[1]._, '1; mode=block', 'value not correct');
+        should.equal(headers.Header[2].$.name, 'X-Content-Type-Options', 'X-Content-Type-Options header not found: ');
+        should.equal(headers.Header[2]._, 'nosniff', 'value not correct');
+        should.equal(headers.Header[3].$.name, 'X-Frame-Options', 'X-Frame-Options header not found: ');
+        should.equal(headers.Header[3]._, 'deny', 'value not correct');
+        done();
+      });
+    });
+
     it('Target should contain header step in PreFlow', function(done) {
       var filePath = path.join(options.destination, options.apiProxy, '/apiproxy/targets/default.xml');
       var fileData = fs.readFileSync(filePath);
@@ -69,6 +93,18 @@ describe('generateApi with headers', function() {
         result.should.have.property('TargetEndpoint').property('PreFlow');
         should.equal(result.TargetEndpoint.PreFlow[0].Request[0].Step[0].Name[0], 'Add AWS api key header', 'Add AWS api key header step in found in PreFlow');
         should.equal(result.TargetEndpoint.PreFlow[0].Request[0].Step[1].Name[0], 'Add token header', 'Add token header step in found in PreFlow');
+        done();
+      });
+    });
+
+    it('Proxy should contain header step in PostFlow', function(done) {
+      var filePath = path.join(options.destination, options.apiProxy, '/apiproxy/proxies/default.xml');
+      var fileData = fs.readFileSync(filePath);
+      var parser = new xml2js.Parser();
+      parser.parseString(fileData, function (err, result) {
+        result.should.have.property('ProxyEndpoint');
+        result.should.have.property('ProxyEndpoint').property('PostFlow');
+        should.equal(result.ProxyEndpoint.PostFlow[0].Response[0].Step[0].Name[0], 'Add security headers', 'Add security headers step in found in PostFlow');
         done();
       });
     });
